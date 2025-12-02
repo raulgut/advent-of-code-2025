b0b2a864c73013707db279825da2419c4a467b8c
Raúl Gutiérrez <raguti@upv.es>
2025-12-02 15:12:24 +0100

    Day 01

---
+++ src/day01p1.hs
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------------
+-- | Day 1 - Part 1
+-- 
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+import Data.List (sort)
+
+-----------------------------------------------------------------------------
+-- Data
+-----------------------------------------------------------------------------
+
+-- | Direction of the combination
+data Direction 
+  = R | L
+  deriving Show             
+
+-- | Instruction
+data Instruction 
+  = Instruction { direction :: Direction 
+                , times :: Int 
+                } deriving Show
+
+-- | State
+data State = State { position :: Int
+                   , zeroVisits :: Int
+                   }
+-- | Main
+main = do
+  args <- getArgs
+  input <- readFile . head $ args
+  let instructions = map toInstruction . lines $ input
+  putStrLn . show . process (State 50 0) $ instructions
+
+-- | From String to Instruction (LX or RX)
+toInstruction :: String -> Instruction
+toInstruction ('L':xs) = Instruction L (read xs)
+toInstruction ('R':xs) = Instruction R (read xs)
+toInstruction str = error $ "Instruction " ++ str ++ " not allowed."
+
+-- | Process the instructions in order
+process :: State -> [Instruction] -> Int
+process st [] = zeroVisits st
+process st ((Instruction L value):is)
+  = let newPosition = (position st) - (mod value 100)
+        adjNewPosition = if (newPosition < 0) then (100 + newPosition) else newPosition
+        newZeroVisits = if (adjNewPosition == 0) then (zeroVisits st) + 1 else (zeroVisits st)
+    in process (State adjNewPosition newZeroVisits) is
+process st ((Instruction R value):is)
+  = let newPosition = (position st) + (mod value 100)
+        adjNewPosition = if (newPosition > 99) then (newPosition - 100) else newPosition
+        newZeroVisits = if (adjNewPosition == 0) then (zeroVisits st) + 1 else (zeroVisits st)
+    in process (State adjNewPosition newZeroVisits) is    
