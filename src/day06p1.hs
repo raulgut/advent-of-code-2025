b7b32dc9d8b7802437ac7ed7af58f2b5a41504ca
Raúl Gutiérrez <raguti@upv.es>
2025-12-07 17:55:30 +0100

    Day 6

---
+++ src/day06p1.hs
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------------
+-- | Day 1 - Part 1
+-- 
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+import Data.List (transpose)
+
+-----------------------------------------------------------------------------
+-- Data
+-----------------------------------------------------------------------------
+
+-- | Main
+main = do
+  args <- getArgs
+  input <- readFile . head $ args
+  let operations = map words . lines $ input
+  putStrLn . show . foldl1 (+) . map (operate . reverse) . transpose $ operations
+
+-- | Apply multiplication or addition
+operate ("*":ns)
+  = foldl1 (*) (map read ns)
+operate ("+":ns)
+  = foldl1 (+) (map read ns)
---
+++ src/day06p2.hs
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------
+-- | Day 1 - Part 2
+-- 
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+import Data.List (transpose)
+
+-----------------------------------------------------------------------------
+-- Data
+-----------------------------------------------------------------------------
+
+-- | Main
+main = do
+  args <- getArgs
+  input <- readFile . head $ args
+  let operations = transpose . Prelude.map (Prelude.filter (/= '\r')) . lines $ input
+  putStrLn . show . foldl1 (+) . operate $ operations
+
+-- | Apply multiplication or addition
+operate :: [String] -> [Int]
+operate [] = []
+operate (n1:ns)
+  = let (n1',op) = (init n1, last n1)
+        (ns1,nss) = span (not . allBlanks) ns 
+    in (case op of 
+          '*' -> foldl1 (*) (map read (n1':ns1))
+          '+' -> foldl1 (+) (map read (n1':ns1))):(if (not . null $ nss) then (operate . tail $ nss) else [])
+
+allBlanks = and . map (==' ')
